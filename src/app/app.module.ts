55e0cc5d077d40787c629a0ef46f7d3ee3a729f2
Aras Navickas <aropastodezute@gmail.com>
2021-07-15 17:51:16 +0300

    pridėjau pacientus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PatientListComponent } from './patient-list/patient-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/patient.ts
@@ -0,0 +1,8 @@
+export class Patient {
+
+  id: number;
+  firstName: string;
+  dateOfBirth: string;
+  telephoneNumber: string;
+
+}
