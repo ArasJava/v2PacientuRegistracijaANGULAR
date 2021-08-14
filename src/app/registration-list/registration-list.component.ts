9d2e62edfa11aac8fe2d40348c739cead84042e7
Aras Navickas <aropastodezute@gmail.com>
2021-08-14 12:31:03 +0300

    pridetas (pries tai praleistas ikeliant i git) registration komponentas

---
+++ src/app/registration-list/registration-list.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import {Patient} from "../patient";
+import {Registration} from "../registration";
+import {RegistrationService} from "../registration.service";
+import {Doctor} from "../doctor";
+import {HttpErrorResponse} from "@angular/common/http";
+
+@Component({
+  selector: 'app-registration-list',
+  templateUrl: './registration-list.component.html',
+  styleUrls: ['./registration-list.component.css']
+})
+export class RegistrationListComponent implements OnInit {
+
+  registrations: Registration[] | undefined;
+
+  constructor(private registrationService: RegistrationService) { }
+
+  ngOnInit(): void {
+  this.getRegistrations()}
+
+  private getRegistrations(): void {
+    this.registrationService.getRegistrations().subscribe(
+      (response:Registration[]) => {
+        this.registrations = response;},
+      (error :HttpErrorResponse) => {
+        alert(error.message);
+      });
+  }
+
+  public onUpdateRegistration(registration: Registration): void {
+    this.registrationService.updateRegistration(registration)
+      .subscribe(
+        (response: Registration) => {
+          // console.log(response);
+          this.registrations = this.registrations?.map((reg)=>reg.id ===response.id ? response : reg)
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
+  }
+
+  public onAddRegistration(registration: Registration): void {
+    this.registrationService.addRegistration(registration)
+      .subscribe(
+        (response: Registration) => {
+          // console.log(response);
+          // @ts-ignore
+          this.registrations = [...this.registrations, response]
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
+  }
+
+  public onDeleteRegistration(registrationId: number): void {
+    this.registrationService.deleteRegistration(registrationId)
+      .subscribe(
+        (response: void) => {
+          // console.log(response);
+          // @ts-ignore
+          this.registrations = this.registrations.filter((registration) =>registrationId !== registrationId)
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
+  }
+
+}
---
+++ src/app/registration.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Registration} from "./registration";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+
+  private baseURL = "http://localhost:8080/registration";
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getRegistrations(): Observable<Registration[]> {
+    return this.httpClient.get<Registration[]>(`${this.baseURL}/all`);
+  }
+
+  public addRegistration(registration :Registration): Observable<Registration> {
+    return this.httpClient.post<Registration>(`${this.baseURL}/add`,registration);
+  }
+
+  public updateRegistration(registration :Registration): Observable<Registration> {
+    return this.httpClient.put<Registration>(`${this.baseURL}/update`,registration);
+  }
+
+  public deleteRegistration(registrationId :number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.baseURL}/delete/${registrationId}`);
+  }
+}
